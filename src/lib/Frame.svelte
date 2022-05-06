d1bcde086a48d1d99a8130d11296499b992bdb6d
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-05-06 16:25:23 +0200

    Add files via upload

--- src/lib/Frame.svelte
+++ src/lib/Frame.svelte
@@ -1,10 +1,14 @@
 <script>
+  export let url
   let content = content
-  fetch("https://raw.githubusercontent.com/Astri2/CoursKatex/main/html/MathsS2-ThmOstrogradsky.html").then(response => {return response.text()}.then(response => {content = response })
+  fetch(url).then(response => {return response.text()}).then(response => {content = response })
+
 </script>
+
 
 {#key content}
   <div>
     {content}
   </div>
 {/key}
+
