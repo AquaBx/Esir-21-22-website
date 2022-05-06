8b1a05899561432ec7d4593fd0640ec93d7753eb
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-05-06 11:55:47 +0200

    Add files via upload

---
+++ src/lib/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+    import Header from "$lib/Header.svelte"
+    export let menu
+    export let path = [menu] 
+    export let i = 0
+    function hide(e){
+        let display = e.target.parentNode.querySelectorAll("ul")
+
+        if (display.length == 0) return
+
+        display = display[0]
+
+        if ( display.style.display == "none"){
+            display.style.display = "block"
+        }
+        else {
+            display.style.display = "none"
+        }
+    } 
+    function change_cours(e){
+        document.querySelector("#iframe_cours").src = e
+    }
+
+    function change_menu(e){
+        i+=1
+        path[i] = e
+        if (e.index){
+            document.querySelector("#iframe_cours").src = e.index
+        }
+    }
+
+    function retour(){
+        delete path[i]
+        i-=1
+    }
+
+</script>
+{#key path}
+<div class="transition">
+    {#if i > 0}
+        <a class="p-5 block hover:bg-[#F7AE50] transition" on:click={retour}>
+            <img class="inline-block align-bottom" src="/svg/arrow.svg">
+            Retour
+        
+        </a>
+    {/if}
+    <ul class="w-48" >
+        { #each Object.keys(path[i]) as categorie }
+            {#if categorie == "links"}
+                { #each Object.keys(path[i][categorie]) as chapitre }
+                    <li class="p-5 hover:bg-[#F7AE50] transition" on:click={change_cours(path[i][categorie][chapitre])}>{chapitre}</li>
+                {/each}
+            {:else if categorie == "categories"}
+                { #each Object.keys(path[i][categorie]) as chapitre }
+                    <li class="p-5 hover:bg-[#F7AE50] transition" on:click={change_menu(path[i][categorie][chapitre])}>{chapitre}</li>
+                {/each}
+            {/if}
+        { /each }
+    </ul>
+</div>
+{/key}
---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Header from "$lib/Header.svelte"
+
+    let matieres = {
+        "categories": {
+            "Prépa 1":{
+                "index":"",
+                "categories":{
+                    "Physique":{
+                        "index":"",
+                        "categories":{
+                        },
+                        "links":{}
+                    },
+                    "Numérique":{
+                        "index":"",
+                        "categories":{},
+                        "links":{}
+                    },
+                    "Mathématiques":{
+                        "index":"",
+                        "categories":{},
+                        "links":{
+                            "Chapitre 1":"https://raw.githubusercontent.com/Astri2/CoursKatex/main/html/MathsS2-ExoInt.html"
+                        }
+                    }
+                }
+            }
+        }        
+    }
+</script>
+<div class="flex bg-[#ebebeb]">
+    <div class="shadow h-[100vh] block" id="menu">
+        <Header  menu={matieres}></Header>
+    </div>
+    <iframe id="iframe_cours" class="w-[100%]"></iframe>
+
+
+</div>
+
+<svelte:head>
+    <title>Tom Chauvel</title>
+    <meta name="description" content="Site de Tom Chauvel / AquaBx, étudiant à l'ESIR à Rennes.">
+
+</svelte:head>
