6b8f8e1ac896431039b57c4935998ee204ca0069
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-05-06 15:09:04 +0200

    Create Frame.svelte

---
+++ src/lib/Frame.svelte
@@ -0,0 +1,10 @@
+<script>
+  let content = content
+  fetch("https://raw.githubusercontent.com/Astri2/CoursKatex/main/html/MathsS2-ThmOstrogradsky.html").then(response => {return response.text()}.then(response => {content = response })
+</script>
+
+{#key content}
+  <div>
+    {content}
+  </div>
+{/key}
