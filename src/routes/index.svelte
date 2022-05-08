65231f737b7f1b1bab1deef21a1b3b600b9a73fb
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-05-08 18:53:22 +0200

    Add files via upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,24 @@
 <script>
+    import Convert from "$lib/Convert.svelte";
     import Header from "$lib/Header.svelte"
     let matieres = {}
     let base_url = "https://raw.githubusercontent.com/Astri2/CoursKatex/main/"
-    
+
     fetch(base_url+"cours.json").then(response => {
 		return response.json()
     }).then(response => {
 		matieres = response 
     })
-
 </script>
 
 <div class="flex">
     {#key matieres}
         <Header base_url={base_url} menu={matieres}></Header>
     {/key}
-    <div id="iframe_content">
-    </div>
+    <Convert/>
 </div>
 
 <svelte:head>
     <title>Cours ESIR</title>
     <meta name="description" content="Site de cours pour L'Esir">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css">
-    <link rel="stylesheet" href="/katex/katex.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/markdown-it-texmath/css/texmath.min.css">
 </svelte:head>
